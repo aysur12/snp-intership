1efb7ce09600383776a2bcc49e2bccd4619f44c0
Aysur Kuchkashev <aysursabin@gmail.com>
2022-11-28 17:56:36 +0700

    исправил цвет при наведении на ссылки в header

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 const deploy = require('gulp-gh-pages');
 
 gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
+  return gulp.src('./dist/**/*')
     .pipe(deploy())
 });
 
