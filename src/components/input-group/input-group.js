9413d75b26e0aa86f2e5aa5d3dffa7208fc786e6
Aysur Kuchkashev <aysursabin@gmail.com>
2022-12-01 16:04:13 +0700

    рефакторинг кода после eslint

--- src/components/input-group/input-group.js
+++ src/components/input-group/input-group.js
@@ -1,6 +1,6 @@
 import jQuery from 'jquery';
 import 'jquery.maskedinput/src/jquery.maskedinput';
 
-jQuery(function($){
-  $("#phone1").mask("+7 (999) 999-99-99");
+jQuery(($) => {
+  $('#phone1').mask('+7 (999) 999-99-99');
 });
